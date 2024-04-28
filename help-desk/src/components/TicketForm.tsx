ac6c95607ea35c4d48befb6aa34e1781f877dee1
Amy Yang <amy.yang7991@gmail.com>
2024-04-28 17:06:21 -0400

    establish post request for submitting tickets

--- help-desk/src/components/TicketForm.tsx
+++ help-desk/src/components/TicketForm.tsx
@@ -1,32 +1,45 @@
-import * as React from "react";
+'use client'
+import * as React from 'react';
 
-const TicketForm: React.FC = () => {
-    const submitTicket = () => {
-        const name = document.getElementById('name');
-        const email = document.getElementById('email');
-        const description = document.getElementById('description');
-
-        fetch('/api/tickets', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(
-                {
-                    name: name,
-                    email: email,
-                    description: description
-                },
-            )
-        })
+const TicketForm = () => {
+    const submitTicket = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const form = e.target as HTMLFormElement;
+        const nameInput = form.elements.namedItem('name') as HTMLInputElement;
+        const emailInput = form.elements.namedItem('email') as HTMLInputElement;
+        const descriptionInput = form.elements.namedItem('description') as HTMLInputElement;
+        
+        const postTicket = async () => {
+            try {
+                const post = await fetch('/api/tickets', {
+                    method: 'POST',
+                    headers: {
+                        "Content-type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        name: nameInput.value,
+                        email: emailInput.value,
+                        description: descriptionInput.value
+                    })
+                });
+                if (post.ok) {
+                    console.log('Ticket successfully submitted');
+                } else {
+                    console.log('Ticket failed to submit');
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        } 
+        postTicket();
     }
 
     return (
-        <form className="flex flex-col">
-            <input type="text" placeholder="name" id='name'/>
-            <input type="text" placeholder="email" id='email' />
-            <textarea placeholder="description" id='description'/>
-            <button type="submit" onSubmit={submitTicket}> Submit </button>
+        <form className='flex flex-col' onSubmit={submitTicket}>
+            <input type='text' placeholder='name' name='name' />
+            <input type='text' placeholder='email' name='email' />
+            <textarea placeholder='description' name='description' />
+            <button type="submit"> Submit </button>
         </form>
     );
 }
