7f1653db91312c49a8ba48d10b5dec9666c2d724
Amy Yang <amy.yang7991@gmail.com>
2024-04-28 20:34:55 -0400

    establish ticket POST request route handling

--- help-desk/src/app/api/tickets/route.ts
+++ help-desk/src/app/api/tickets/route.ts
@@ -3,7 +3,20 @@ import prisma from '../../../../prisma/prisma';
 
 export async function POST(req: Request) {
     const ticketData = await req.json();
-    console.log(ticketData);
-    
-    return NextResponse.json(200);
+    const { name, email, description } = ticketData;
+
+    try {
+        const ticket = await prisma.ticket.create({
+            data: {
+                name: name,
+                email: email,
+                description: description,
+                status: "OPEN"
+            }
+        });
+        return NextResponse.json(ticket, {status: 201});
+    } catch (error) {
+        console.log('Ticket failed to create: ', error);
+        return NextResponse.json({ error: 'Internal Server Error'}, { status: 500 });
+    }
 }
