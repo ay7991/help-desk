0c6f09c74007f1352a3a345d95d7048d6dadd2e7
Amy Yang <amy.yang7991@gmail.com>
2024-04-29 16:26:46 -0400

    create reusable ticket component

---
+++ help-desk/src/components/Ticket.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+interface TicketProps {
+    id: number,
+    name: string,
+    email: string,
+    description: string,
+    status: string
+}
+
+const Ticket: React.FC<TicketProps> = ({id, name, email, description, status}) => {
+    
+    return (
+        <main>
+            <h1> Ticket: {id}</h1>
+            <h2> Name: {name} </h2>
+            <h2> Email: {email} </h2>
+            <p> Description: {description} </p>
+            <select>
+                <option> OPEN </option>
+                <option> IN PROGRESS </option>
+                <option> RESOLVED </option>
+                <option> CLOSED </option>
+            </select>
+        </main>
+    );
+}
+
+export default Ticket;
