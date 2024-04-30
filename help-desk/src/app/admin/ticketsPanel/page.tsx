53844708d1d4cca617a72632e27c5cd84c840754
Amy Yang <amy.yang7991@gmail.com>
2024-04-29 21:04:00 -0400

    include cookie authentication for admin users

--- help-desk/src/app/admin/ticketsPanel/page.tsx
+++ help-desk/src/app/admin/ticketsPanel/page.tsx
@@ -1,8 +1,26 @@
 import * as React from 'react';
+import { cookies } from 'next/headers';
 
-const TicketsPanel: React.FC = () => {
+const TicketsPanel: React.FC = async () => {
+    const cookieStore = cookies();
+    const adminCookie = cookieStore.get('adminCookie');
+    const Tickets: JSX.Element[] = [];
+
+    if (!adminCookie) {
+        return <h1> You do not have access to view this page. </h1>;
+    } else {
+        try {
+            const response = await fetch('http://localhost:3000/api/tickets');
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
-        <h1> Tickets Panel </h1>
+        <main>
+            <h1> Tickets Panel </h1>
+            {Tickets}
+        </main>
     );
 };
 
