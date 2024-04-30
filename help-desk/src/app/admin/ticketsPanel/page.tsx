f9cd2afe636f17a93e9535333424a1e99f873bde
Amy Yang <amy.yang7991@gmail.com>
2024-04-30 00:51:41 -0400

    render ticket components into ticketpanel

--- help-desk/src/app/admin/ticketsPanel/page.tsx
+++ help-desk/src/app/admin/ticketsPanel/page.tsx
@@ -1,16 +1,30 @@
 import * as React from 'react';
 import { cookies } from 'next/headers';
+import Ticket from '@/components/Ticket';
 
 const TicketsPanel: React.FC = async () => {
     const cookieStore = cookies();
     const adminCookie = cookieStore.get('adminCookie');
-    const Tickets: JSX.Element[] = [];
+    const tickets: JSX.Element[] = [];
 
     if (!adminCookie) {
         return <h1> You do not have access to view this page. </h1>;
     } else {
         try {
-            const response = await fetch('http://localhost:3000/api/tickets');
+            const getTickets = await fetch('http://localhost:3000/api/tickets');
+            const ticketData = await getTickets.json();
+            for (let i = 0; i < ticketData.length; i++) {
+                tickets.push(<Ticket
+                    key={`ticket${i}`}
+                    id={ticketData[i].id} 
+                    name={ticketData[i].name} 
+                    email={ticketData[i].email}
+                    description={ticketData[i].description}
+                    createdAt={ticketData[i].createdAt}
+                    updatedAt={ticketData[i].updatedAt}
+                    status={ticketData[i].status}
+                />)
+            }
         } catch (error) {
             console.log(error);
         }
@@ -19,7 +33,7 @@ const TicketsPanel: React.FC = async () => {
     return (
         <main>
             <h1> Tickets Panel </h1>
-            {Tickets}
+            {tickets}
         </main>
     );
 };
