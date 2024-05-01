627061c659d67ca2697863f1ceb765e9f67331d3
Amy Yang <amy.yang7991@gmail.com>
2024-05-01 17:55:41 -0400

    modularize post request to submit ticket

--- help-desk/src/components/TicketForm.tsx
+++ help-desk/src/components/TicketForm.tsx
@@ -2,48 +2,85 @@
 import * as React from 'react';
 
 const TicketForm = () => {
-    const submitTicket = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const form = e.target as HTMLFormElement;
-        const nameInput = form.elements.namedItem('name') as HTMLInputElement;
-        const emailInput = form.elements.namedItem('email') as HTMLInputElement;
-        const descriptionInput = form.elements.namedItem('description') as HTMLInputElement;
-        
+    const [name, setName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [description, setDescription] = React.useState('');
+
+    const postTicket = async (): Promise<void> => {
         try {
+            if (name === '') {
+                alert('Do not leave "Name" empty');
+                throw new Error("Name field empty");
+            }
+
+            if (email === '') {
+                alert('Do not leave "Email" empty');
+                throw new Error("Email field empty");
+            }
+
+            if (description === '') {
+                alert('Do not leave "Description" empty');
+                throw new Error("Description field empty");
+            }
+
             const post = await fetch('/api/tickets', {
                 method: 'POST',
                 headers: {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify({
-                    name: nameInput.value,
-                    email: emailInput.value,
-                    description: descriptionInput.value
+                    name: name,
+                    email: email,
+                    description: description
                 })
             });
             if (post.ok) {
-                console.log('Ticket successfully submitted');
-            } else {
-                console.log('Ticket failed to submit');
+                alert('Ticket successfully submitted');
             }
         } catch (error) {
+            alert('Failed to submit ticket');
             console.log(error);
         }
+    }
+
+    const submitTicket = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        postTicket();
     }
 
     return (
         <form className="flex flex-col w-96" onSubmit={submitTicket}>
             <label className="flex flex-col"> 
                 Name 
-                <input className="border-solid border-black border-2 mb-4 pl-1" type='text' placeholder='Name' name='name' />
+                <input 
+                    className="border-solid border-black border-2 mb-4 pl-1" 
+                    type='text' 
+                    placeholder='Name' 
+                    name='name' 
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                />
             </label> 
             <label className="flex flex-col">
                 Email
-                <input className="border-solid border-black border-2 mb-4 pl-1" type='text' placeholder='Email' name='email' />
+                <input 
+                    className="border-solid border-black border-2 mb-4 pl-1" 
+                    type='email' 
+                    placeholder='Email' 
+                    name='email' 
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
             </label>
             <label className="flex flex-col"> 
                 Description 
-                <textarea className="border-solid border-black border-2 mb-4 pl-1 h-36" placeholder='Explain the problem' name='description' />
+                <textarea 
+                    className="border-solid border-black border-2 mb-4 pl-1 h-36" 
+                    placeholder='Explain the problem' 
+                    name='description'
+                    value={description}
+                    onChange={e => setDescription(e.target.value)} 
+                />
             </label>
             <button type="submit"> Submit </button>
         </form>
