6b440b8d86b7601552bf9adab51358ef68a7560c
Amy Yang <amy.yang7991@gmail.com>
2024-05-01 10:53:00 -0400

    include navbar and remove original admin route

--- help-desk/src/app/page.tsx
+++ help-desk/src/app/page.tsx
@@ -1,13 +1,16 @@
 import TicketForm from "@/components/TicketForm";
+import NavBar from "@/components/NavBar";
 
-const Home = () => {
+const Help = () => {
   return (
-    <main className="h-screen flex flex-col items-center justify-center">
-      <h1> Submit a Ticket </h1>
-      <TicketForm />
-      <a href='/admin/login'> Admin Login </a>
+    <main className="h-screen">
+      <NavBar />
+      <section className="h-4/5 flex flex-col items-center justify-center">
+        <h1 className="mb-8 text-xl"> Submit Your Ticket </h1>
+        <TicketForm />
+      </section>
     </main>
   );
 }
 
-export default Home;
+export default Help;
