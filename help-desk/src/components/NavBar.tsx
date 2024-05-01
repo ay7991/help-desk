4a0cc03e589b75ad691ad6c2580b2a233f425be6
Amy Yang <amy.yang7991@gmail.com>
2024-05-01 10:56:18 -0400

    create navbar component

---
+++ help-desk/src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Link from 'next/link';
+
+const NavBar: React.FC = () => {
+    return (
+        <nav className="flex justify-end pr-20 pt-4">
+            <Link href='/admin/login'> Admin </Link>
+            <Link href='/' className="ml-12"> Help </Link>
+        </nav>
+    );
+}
+
+export default NavBar;
