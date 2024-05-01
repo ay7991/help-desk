afc54088aebada3f6b3c2b6ce7a5f3d50d2c38a2
Amy Yang <amy.yang7991@gmail.com>
2024-05-01 17:47:21 -0400

    rename nav link

--- help-desk/src/components/NavBar.tsx
+++ help-desk/src/components/NavBar.tsx
@@ -5,7 +5,7 @@ const NavBar: React.FC = () => {
     return (
         <nav className="flex justify-end pr-20 pt-4">
             <Link href='/admin/login'> Admin </Link>
-            <Link href='/' className="ml-12"> Help </Link>
+            <Link href='/' className="ml-12"> Submit Tickets </Link>
         </nav>
     );
 }
