bf0aaae4321f72f02245ab1edde637aef11adcee
Amy Yang <amy.yang7991@gmail.com>
2024-05-09 22:58:53 -0400

    remove tailwind and add id for some element for css

--- help-desk/src/app/page.tsx
+++ help-desk/src/app/page.tsx
@@ -6,7 +6,7 @@ const Home = () => {
     <main className="h-screen">
       <NavBar />
       <section className="h-4/5 flex flex-col items-center justify-center">
-        <div className="flex flex-col items-center justify-center border-solid border-2 w-1/3 h-3/4 rounded-xl bg-blue-500">
+        <div id="divTicketForm">
           <h1 className="mb-8 text-3xl text-white"> Submit Your Ticket </h1>
           <TicketForm />
         </div>
