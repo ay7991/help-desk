502b344e20cfcd66b19d697811e93b0817236a13
Amy Yang <amy.yang7991@gmail.com>
2024-05-10 18:18:07 -0400

    adjust class names for css

--- help-desk/src/app/page.tsx
+++ help-desk/src/app/page.tsx
@@ -5,8 +5,8 @@ const Home = () => {
   return (
     <main className="h-screen">
       <NavBar />
-      <section className="h-4/5 flex flex-col items-center justify-center">
-        <div id="divTicketForm">
+      <section className="formSection">
+        <div className="formDiv">
           <h1 className="mb-8 text-3xl text-white"> Submit Your Ticket </h1>
           <TicketForm />
         </div>
