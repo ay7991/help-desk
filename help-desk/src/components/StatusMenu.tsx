fc1ad74b01b7006672c56eb2f701d11906fb9dd1
Amy Yang <amy.yang7991@gmail.com>
2024-05-10 21:03:35 -0400

    transfer functionality to utils file

--- help-desk/src/components/StatusMenu.tsx
+++ help-desk/src/components/StatusMenu.tsx
@@ -4,58 +4,19 @@ import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { StatusProps } from '@/lib/types';
 import Notification from './Notification';
+import { useStatusUpdate } from '@/lib/utils/statusMenu';
 
 const StatusMenu: React.FC<StatusProps> = ({ currStatus, currID }) => {
-  const [status, setStatus] = React.useState(currStatus);
-  const [showNotif, setShowNotif] = React.useState(false);
-  const [message, setMessage] = React.useState('');
-  const [color, setColor] = React.useState('');
+  const { status, setStatus, showNotif, setShowNotif, message, color } = useStatusUpdate(currStatus, currID);
 
-  React.useEffect(() => {
-    const patchFetch = async (): Promise<void> => {
-      try {
-        const statusUpdate = await fetch('/api/tickets', {
-            method: 'PATCH',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                id: currID,
-                status: status
-            })
-        });
-  
-        if (statusUpdate.ok) {
-            setShowNotif(true);
-            setMessage('Status Successfully Updated!');
-            setColor('teal');
-            console.log(color);
-        } else {
-            const response = await statusUpdate.json();
-            setShowNotif(true);
-            setMessage(response.error);
-            setColor('red');
-            throw new Error(response.error);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    if (status !== currStatus) { 
-      patchFetch();
-    }
-  }, [status, currID, currStatus, color]);
-
-
-  const updateStatus = (event: SelectChangeEvent) => {
-    setStatus(event.target.value);
-  }
+  const handleStatusChange = (event: SelectChangeEvent) => {
+    setStatus(event.target.value as string);
+  };
 
   return (
     <>
       <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-        <Select sx={{color: '#3B82F6'}} value={status} onChange={updateStatus}>
+        <Select sx={{color: '#3B82F6'}} value={status} onChange={handleStatusChange}>
           <MenuItem sx={{color: '#3B82F6'}} value={'OPEN'}> OPEN </MenuItem>
           <MenuItem sx={{color: '#3B82F6'}} value={'IN PROGRESS'}> IN PROGRESS </MenuItem>
           <MenuItem sx={{color: '#3B82F6'}} value={'RESOLVED'}> RESOLVED </MenuItem>
